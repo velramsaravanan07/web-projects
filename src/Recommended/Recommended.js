e6f81789ec17adcce22caeddb77ed53877856e4b
VELRAM_._ <145835291+velramsaravanan07@users.noreply.github.com>
2023-11-26 13:25:43 +0530

    Recommended.js

--- src/Recommended/Recommended.js
+++ src/Recommended/Recommended.js
@@ -5,8 +5,8 @@ import "./Recommended.css";
 const Recommended = ({ handleClick }) => {
   return (
     <>
-      <div>
-        <h2 className="recommended-title">Recommended</h2>
+      
+        <h2 className="recommended-title">Recommended...</h2>
         <div className="recommended-flex">
           <Button onClickHandler={handleClick} value="" title="All Products"/>
           <Button onClickHandler={handleClick} value="Nike" title="Nike" />
@@ -14,7 +14,7 @@ const Recommended = ({ handleClick }) => {
           <Button onClickHandler={handleClick} value="Puma" title="Puma"/>
           <Button onClickHandler={handleClick} value="Vans" title="Vans"/>
         </div>
-      </div>
+      
     </>
   );
 };
